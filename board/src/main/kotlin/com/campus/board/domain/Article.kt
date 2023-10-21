0914617a1fde60a5947e5197688a5fef2db3ff54
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2023-10-21 16:17:51 +0900

    #8 주석처리하기
    주석을 처리했습니다.

--- board/src/main/kotlin/com/campus/board/domain/Article.kt
+++ board/src/main/kotlin/com/campus/board/domain/Article.kt
@@ -2,9 +2,15 @@ package com.campus.board.domain
 
 import java.time.LocalDateTime
 
-class Article(val id : Long,var title : String,var hashTag : String,var content : String) {
-    var createdAt : LocalDateTime = LocalDateTime.now()
-    var createdBy : String = ""
-    var modifiedAt : LocalDateTime? = null
-    var modifiedBy : String? = ""
+/**
+ * id : 아이디
+ * title : 제목
+ * hashTag : 해시태그
+ * content 본문
+ */
+class Article(var id : Long,var title : String,var hashTag : String,var content : String) {
+    var createdAt : LocalDateTime = LocalDateTime.now() //생성일
+    var createdBy : String = "" //생성자
+    var modifiedAt : LocalDateTime? = null //수정일
+    var modifiedBy : String? = "" //수정자
 }
--- board/src/main/kotlin/com/campus/board/domain/ArticleComment.kt
+++ board/src/main/kotlin/com/campus/board/domain/ArticleComment.kt
@@ -2,9 +2,15 @@ package com.campus.board.domain
 
 import java.time.LocalDateTime
 
+/**
+ * 댓글 도메인
+ * id : 아이디
+ * article : 게시글
+ * content : 본문
+ */
 class ArticleComment(var id : Long,var article : Article,var content : String) {
-    var createdAt : LocalDateTime = LocalDateTime.now()
-    var createdBy : String = ""
-    var modifiedAt : LocalDateTime? = null
-    var modifiedBy : String? = ""
+    var createdAt : LocalDateTime = LocalDateTime.now() //생성일
+    var createdBy : String = "" //생성자
+    var modifiedAt : LocalDateTime? = null //수정일
+    var modifiedBy : String? = "" //수정자
 }
