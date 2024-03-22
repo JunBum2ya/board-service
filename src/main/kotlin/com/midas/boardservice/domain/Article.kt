d14acb03f647e821c2093884b87a9f65365c7e03
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-03-22 12:17:08 +0900

    [feat] add article domain
    게시글 도메인 추가

--- src/main/kotlin/com/midas/boardservice/domain/Article.kt
+++ src/main/kotlin/com/midas/boardservice/domain/Article.kt
@@ -1,4 +1,35 @@
 package com.midas.boardservice.domain
 
-class Article {
+import jakarta.persistence.*
+import org.hibernate.annotations.Comment
+
+@Entity
+class Article(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    @Comment("대체키")
+    private var id: Long? = null,
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    val member: Member,
+    @Column(nullable = false, length = 100) private var title: String,
+    @Column(nullable = false, length = 10000) private var content: String
+) : BaseEntity() {
+    fun update(title: String? = null, content: String? = null) {
+        title?.let { this.title = it }
+        content?.let { this.content = it }
+    }
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Article
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
 }
