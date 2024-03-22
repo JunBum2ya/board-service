30499ba178917a33a2880b434666ad72fd452741
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-03-22 10:57:16 +0900

    Initial commit

---
+++ src/test/kotlin/com/midas/boardservice/BoardServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.midas.boardservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
