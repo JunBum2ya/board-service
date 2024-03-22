1315c6fddee1c6e5810a4c76b6d905d89ebe85d8
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-03-22 12:03:40 +0900

    [feat] add member domain
    member 도메인 구현

--- src/main/kotlin/com/midas/boardservice/domain/Member.kt
+++ src/main/kotlin/com/midas/boardservice/domain/Member.kt
@@ -1,4 +1,45 @@
 package com.midas.boardservice.domain
 
-class Member {
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.hibernate.annotations.Comment
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Entity
+class Member(
+    @Id @Column(nullable = false, length = 30) @Comment("사용자 아이디") private var id: String,
+    @Column(nullable = false, length = 50, unique = true) @Comment("이메일") private var email: String,
+    @Column(nullable = false, length = 30) @Comment("닉네임") private var nickname: String,
+    @Column(nullable = false) @Comment("패스워드") private var password: String,
+    private var memo: String?
+): BaseEntity() {
+
+    /**
+     * 필드 수정
+     */
+    fun update(email: String? = null, nickname: String? = null, memo: String? = null) {
+        email?.let { this.email = it }
+        nickname?.let { this.nickname = it }
+        memo?.let { this.memo = it }
+    }
+
+    /**
+     * 패스워드 수정
+     */
+    fun updatePassword(password: String, passwordEncoder: PasswordEncoder) {
+        this.password = passwordEncoder.encode(password)
+    }
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Member
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
