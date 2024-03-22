06e3f1c800fe04e94c8a5982b7ba1274145c91d5
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-03-22 11:26:32 +0900

    [feat] add common column
    BaseEntity 구현 (등록일시,등록자,수정일시, 수정자)

--- src/main/kotlin/com/midas/boardservice/domain/BaseEntity.kt
+++ src/main/kotlin/com/midas/boardservice/domain/BaseEntity.kt
@@ -1,4 +1,37 @@
 package com.midas.boardservice.domain
 
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.Comment
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import org.springframework.format.annotation.DateTimeFormat
+import java.time.LocalDateTime
+
+@EntityListeners(AuditingEntityListener::class)
+@MappedSuperclass
 class BaseEntity {
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    @Comment("등록일시")
+    protected var createdAt: LocalDateTime? = null
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 30)
+    @Comment("등록자")
+    protected var createdBy: String? = null
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false, updatable = false)
+    @Comment("최종수정일시")
+    protected var updatedAt: LocalDateTime? = null
+    @LastModifiedBy
+    @Column(nullable = false, updatable = false, length = 30)
+    @Comment("최종수정자")
+    protected var updatedBy: String? = null
+
 }
