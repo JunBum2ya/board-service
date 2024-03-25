327ae2623ace6d40221e6564c1eab54b0567b256
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-03-25 12:16:16 +0900

    [feat] pagination test
    페이지네이션 비즈니스 로직 구현

---
+++ src/main/kotlin/com/midas/boardservice/service/PaginationService.kt
@@ -0,0 +1,19 @@
+package com.midas.boardservice.service
+
+import org.springframework.stereotype.Service
+import java.util.stream.Collectors
+import java.util.stream.IntStream
+
+@Service
+class PaginationService {
+    private val BAR_LENGTH = 10
+
+    /**
+     * 페이지 리스트 계산 후 반환
+     */
+    fun getPaginationBarNumbers(currentPageNumber: Int, totalPages: Int): List<Int> {
+        val startNumber = (currentPageNumber - (BAR_LENGTH / 2)).coerceAtLeast(0)
+        val endNumber = (startNumber + BAR_LENGTH).coerceAtMost(totalPages)
+        return IntStream.range(startNumber,endNumber).boxed().collect(Collectors.toList())
+    }
+}
