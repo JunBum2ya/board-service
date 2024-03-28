252ebd13a12be52f1606aef7b959895f445fbd81
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-03-28 12:00:48 +0900

    [feat] connect root url
    root url을 게시글 리스트페이지와 연결

--- src/main/kotlin/com/midas/boardservice/controller/MainController.kt
+++ src/main/kotlin/com/midas/boardservice/controller/MainController.kt
@@ -1,4 +1,13 @@
 package com.midas.boardservice.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class MainController {
+
+    @GetMapping("/")
+    fun root(): String {
+        return "forward:/articles"
+    }
 }
