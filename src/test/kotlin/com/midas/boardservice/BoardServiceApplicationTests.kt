ccc16441181994e966de5f15ad9c839ad9ed62a2
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-10-13 11:11:54 +0900

    [test] 테스트 오류 수정
    인증 시 파라미터 오류 수정

--- src/test/kotlin/com/midas/boardservice/BoardServiceApplicationTests.kt
+++ src/test/kotlin/com/midas/boardservice/BoardServiceApplicationTests.kt
@@ -1,11 +1,15 @@
 package com.midas.boardservice
 
+import com.midas.boardservice.config.TestSecurityConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
 @ActiveProfiles("testdb")
+@Import(TestSecurityConfig::class)
 class BoardServiceApplicationTests {
 
     @Test
--- src/test/kotlin/com/midas/boardservice/config/TestSecurityConfig.kt
+++ src/test/kotlin/com/midas/boardservice/config/TestSecurityConfig.kt
@@ -1,8 +1,10 @@
 package com.midas.boardservice.config
 
 import com.midas.boardservice.member.service.MemberService
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
 import org.springframework.test.context.event.annotation.BeforeTestMethod
 
 @Import(SecurityConfig::class)
@@ -10,6 +12,9 @@ class TestSecurityConfig {
 
     @MockBean
     private lateinit var memberService: MemberService
+
+    @MockBean
+    private lateinit var clientRegistrationRepository: InMemoryClientRegistrationRepository
 
     @BeforeTestMethod
     fun securitySetup() {
